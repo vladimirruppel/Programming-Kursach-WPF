6ca141934577ae0188a45615fb18ab6d53d03ee7
vladimirruppel <vladimir.ruppel2004@mail.ru>
2024-01-10 11:24:26 +0700

    Добавление папки "Model" и класса "Track"

---
+++ Model/Track.cs
@@ -0,0 +1,10 @@
+﻿namespace prog3_kursach.Model
+{
+    internal class Track
+    {
+        public string ArtistName { get; set; }
+        public string TrackName { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Path{ get; set; }
+    }
+}

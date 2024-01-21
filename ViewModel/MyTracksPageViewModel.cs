256649ba5e194ec0a68b3362b308c5ba6f6a7c1f
vladimirruppel <vladimir.ruppel2004@mail.ru>
2024-01-21 15:16:40 +0700

    Сделана страница "Мои треки", содержащая лайкнутые пользователем треки

---
+++ ViewModel/MyTracksPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prog3_kursach.Model;
+using prog3_kursach.MVVM;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace prog3_kursach.ViewModel
+{
+    class MyTracksPageViewModel : ViewModelBase
+    {
+        ApplicationContext db = new ApplicationContext();
+
+        private readonly ObservableCollection<TrackViewModel> tracks;
+        public IEnumerable<TrackViewModel> Tracks => tracks;
+
+        public MyTracksPageViewModel()
+        {
+            tracks = new ObservableCollection<TrackViewModel>();
+
+            LoadTrackFromDB();
+        }
+
+        private void LoadTrackFromDB()
+        {
+            db.Database.EnsureCreated();
+            db.Tracks.Load();
+
+            foreach (Track track in db.Tracks)
+            {
+                if (track.IsAdded) 
+                    tracks.Add(new TrackViewModel(track));
+            }
+        }
+    }
+}

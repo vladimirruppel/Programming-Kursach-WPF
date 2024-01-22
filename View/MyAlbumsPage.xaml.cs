f33645b1720488fdbee628206f9252f75bd907d4
vladimirruppel <vladimir.ruppel2004@mail.ru>
2024-01-22 18:03:31 +0700

    Добавлена страница "Мои альбомы" (сейчас она отображает все созданные альбомы, а не только добавленные)

---
+++ View/MyAlbumsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using prog3_kursach.ViewModel;
+using System.Windows.Controls;
+
+namespace prog3_kursach.View
+{
+    public partial class MyAlbumsPage : Page
+    {
+        public MyAlbumsPage()
+        {
+            InitializeComponent();
+            DataContext = new MyAlbumsPageViewModel();
+        }
+    }
+}
---
+++ ViewModel/MyAlbumsPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prog3_kursach.Model;
+using prog3_kursach.MVVM;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace prog3_kursach.ViewModel
+{
+    public class MyAlbumsPageViewModel : ViewModelBase
+    {
+        private ApplicationContext db = new ApplicationContext();
+
+        private ObservableCollection<AlbumViewModel> albums = new ObservableCollection<AlbumViewModel>();
+        public IEnumerable<AlbumViewModel> Albums => albums;
+
+        public MyAlbumsPageViewModel()
+        {
+            db.Database.EnsureCreated();
+            db.Albums.Load();
+
+            foreach (Album album in db.Albums.ToList()) {
+                albums.Add(new AlbumViewModel(album));
+            }
+        }
+    }
+}

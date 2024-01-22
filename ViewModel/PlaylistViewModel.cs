eee34f0315e2ce0b3d3a4280bed311ab02290ec3
vladimirruppel <vladimir.ruppel2004@mail.ru>
2024-01-22 18:43:27 +0700

    Реализовано отображение плейлиста

---
+++ ViewModel/PlaylistViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prog3_kursach.Model;
+using prog3_kursach.MVVM;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace prog3_kursach.ViewModel
+{
+    public class PlaylistViewModel : ViewModelBase
+    {
+        private ApplicationContext db = new ApplicationContext();
+
+        private Playlist playlist;
+        public Playlist Playlist
+        {
+            get { return playlist; }
+            set
+            {
+                playlist = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PlaylistName => Playlist.Name;
+        public bool IsAdded => Playlist.IsAdded;
+
+        public List<TrackViewModel> Tracks => GetTracksOfPlaylistFromDB(Playlist);
+
+        public RelayCommand TogglePlaylistCommand { get; }
+
+        public PlaylistViewModel(Playlist playlist)
+        {
+            Playlist = playlist;
+
+            db.Database.EnsureCreated();
+            db.Playlists.Load();
+            db.Tracks.Load();
+            db.PlaylistsTracks.Load();
+
+            TogglePlaylistCommand = new RelayCommand(execute => TogglePlaylist());
+        }
+
+        private List<TrackViewModel> GetTracksOfPlaylistFromDB(Playlist playlist)
+        {
+            List<TrackViewModel> tracks = new List<TrackViewModel>();
+
+            List<PlaylistTrack> playlistTracks = db.PlaylistsTracks
+                .Where(el => el.PlaylistId == playlist.Id).ToList();
+
+            foreach (PlaylistTrack playlistTrack in playlistTracks)
+            {
+                Track track = db.Tracks.Find(playlistTrack.TrackId);
+                tracks.Add(new TrackViewModel(track));
+            }
+
+            return tracks;
+        }
+
+        private void TogglePlaylist()
+        {
+            Playlist playlist = db.Playlists.Find(Playlist.Id);
+            playlist.IsAdded = !playlist.IsAdded;
+            db.SaveChanges();
+
+            Playlist = playlist;
+        }
+    }
+}
